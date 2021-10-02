bb809568b3b63ed0ae9e95242de65afb8f55f9e5
Pranav <81732612+Pranav5537@users.noreply.github.com>
2021-10-02 22:15:43 +0530

    1

---
+++ script.js
@@ -0,0 +1,27 @@
+
+document.getElementById("next").addEventListener("click",getData)
+
+let image = document.getElementById("image")
+
+
+async function getData()
+{
+
+    try{
+        const res = await fetch("https://api.thecatapi.com/v1/images/search ");
+
+        const data = await res.json();       
+    
+        console.log(data[0].url);
+
+    
+    //  variable to display image in DOM
+
+        output =`${data[0].url}`    
+        document.querySelector("#output").src = output ;
+
+    } catch (error){
+        console.log(error, " unable to fetch data")
+    }
+    
+}
